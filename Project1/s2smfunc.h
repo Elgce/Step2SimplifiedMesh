391e4137f2b9ffaccc6e81f0239b0c488185e3e0
Elgce <bqw20@mails.tsinghua.edu.cn>
2022-03-13 20:57:14 +0800

    add extern C

--- Project1/s2smfunc.h
+++ Project1/s2smfunc.h
@@ -1,3 +1,3 @@
 #pragma once
 #include "step2spedstl.h"
-void __declspec(dllexport) step2simplifiedmesh(const std::string& fileName, int simplifyFlag, double percent, const char* tmpPath, const char* outPath);
+extern "C" void __declspec(dllexport) step2simplifiedmesh(const std::string & fileName, int simplifyFlag, double percent, const char* tmpPath, const char* outPath);
